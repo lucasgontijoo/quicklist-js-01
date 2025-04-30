b29974892fbe312064e7e0fffce4228e319701c8
Lucas Gontijo da Cunha <lucasgontijoo470@gmail.com>
2025-04-30 14:45:18 -0300

    função de adicionar item na lista implementado e início da funcionalidade de remover item

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,23 +1,40 @@
 const form = document.querySelector('.add-item')
-const btn = document.querySelector('.btn-add-item')
 const input = document.querySelector('#input-add-item')
 const ul = document.querySelector('.list-items')
 
-form.addEventListener("submit", (event) => {
+form.addEventListener('submit', (event) => {
     event.preventDefault()
 
-    const inputValue = input.value
-    const newLi = document.createElement("li")
-    const div = document.createElement("div")
+    let inputValue = input.value
+    const newLi = document.createElement('li')
+    const div = document.createElement('div')
     div.classList.add('list-item-init-content')
-    const input = document.createElement("input")
-    const label = document.createElement("label")
-    label.innerText = inputValue
-    
-    ul.appendChild(newLi)
-    newLi.appendChild(div)
-    div.appendChild(input)
-    div.appendChild(label)
+    const inputCheck = document.createElement('input')
+    inputCheck.type = 'checkbox'
+    const labelLi = document.createElement('label')
+    labelLi.innerText = inputValue
+    const imgLi = document.createElement('img')
+    imgLi.src = './assets/img/icon_delete.svg'
+    imgLi.classList.add('icon-delete')
 
-    alert('passa aqui')
+    if (inputValue == "") {
+        alert("Não é possível adicionar um produto sem nome.")
+    } else {
+        ul.appendChild(newLi)
+        newLi.append(div, imgLi)
+        div.append(inputCheck, labelLi)
+        
+        input.value = ""
+    }
+})
+
+const deleteIcon = document.querySelector('.icon-delete')
+
+deleteIcon.addEventListener('click', (event) => {
+    event.preventDefault()
+    console.log('teste')
+
+    ul.removeChild(newLi)
+    newLi.remove(div, imgLi)
+    div.remove(inputCheck, labelLi)
 })
